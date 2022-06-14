23e7b05a5342ee10f6d28d58ffe43748f74ece63
Andrii Boiadzhi <aboiadzh@cisco.com>
2022-06-14 16:34:20 +0300

    last changes

--- nest_example/src/pipes/validation.pipe.ts
+++ nest_example/src/pipes/validation.pipe.ts
@@ -1,8 +1,21 @@
-import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import {
+  ArgumentMetadata,
+  HttpException,
+  Injectable,
+  PipeTransform,
+} from "@nestjs/common";
+import { plainToClass } from "class-transformer";
+import { validate } from "class-validator";
 
 @Injectable()
 export class ValidationPipe implements PipeTransform {
-  transform(value: any, metadata: ArgumentMetadata) {
+  async transform(value: any, metadata: ArgumentMetadata) {
+    const obj = plainToClass(metadata.metatype, value);
+    const errors = await validate(obj);
+
+    if (errors.length) {
+      throw new HttpException();
+    }
     return value;
   }
 }
--- nest_example/src/user/dto/create-user.dto.ts
+++ nest_example/src/user/dto/create-user.dto.ts
@@ -1,8 +1,13 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsString, Length } from "class-validator";
 
 export class CreateUserDto {
-  @ApiProperty({ example: 'example@mail.com', description: "User email" })
+  @ApiProperty({ example: "example@mail.com", description: "User email" })
+  @IsString({ message: "Should be a string" })
+  @IsEmail({}, { message: "Should be email" })
   readonly email: string;
-  @ApiProperty({ example: '12345', description: "User password" })
+  @ApiProperty({ example: "12345", description: "User password" })
+  @IsString({ message: "Should be a string" })
+  @Length(4, 16, { message: "Should be longer than 4 symbols" })
   readonly password: string;
 }
